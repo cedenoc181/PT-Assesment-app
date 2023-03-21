d3a5a3f4129b6f2db40230b936a8d53823e2ecdd
Christian <cedenoc181@gmail.com>
2023-03-21 00:06:42 -0400

    added full crud methods to pt user controller

--- app/controllers/pt_users_controller.rb
+++ app/controllers/pt_users_controller.rb
@@ -2,11 +2,35 @@ class PtUsersController < ApplicationController
 
     def index 
         users = PtUser.all
-        render :json users, status: :ok
+        render json: users, status: :ok
     end
 
-    
+    def show 
+        user = find_user
+        render json: user
+    end
 
+    def create 
+        new_user = PtUSer.create!(create_user_params)
+        render json: new_user, status: :created
+    end
+
+    def update 
+        update_user = find_user 
+        update_user.update!(update_user_params)
+        render json: update_user, status: :updated 
+     end
+
+     def destroy
+        del_user = find_user 
+        del_user.destroy
+        head :no_content 
+     end
+
+
+     private 
+
+     
 
 
 
